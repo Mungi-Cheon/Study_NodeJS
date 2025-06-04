c6841c4ac275884d0668352d952b0b45264699fa
Mungi Cheon <c.mungi7421@gmail.com>
2025-06-04 19:25:51 +0900

    study: MyBlog 게시물 리스트, 특정 게시물 조회 구현

--- myBlog/routes/main.js
+++ myBlog/routes/main.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs";
 const Post = require("../models/Post");
+const asyncHandler = require("express-async-handler");
 
-router.get(["/","/home"], (req, res) => {
+
+router.get(["/","/home"], asyncHandler( async (req, res) => {
     const locals = {
         title: "Home"
     }
-    res.render("index", {locals: locals, layout: mainLayout});
-});
+
+    const data = await Post.find();
+    res.render("index", {locals: locals, data, layout: mainLayout});
+}));
 
 router.get("/about", (req, res) => {
     const locals = {
@@ -16,5 +20,15 @@ router.get("/about", (req, res) => {
     }
     res.render("about", {locals, layout: mainLayout});
 });
+
+/**
+ * 게시물 상세 보기
+ * Get /posts/:id
+ */
+router.get("/posts/:id", asyncHandler( async (req, res) => {
+    const data = await Post.findOne({_id: req.params.id});
+    res.render("post", {data, layout: mainLayout});
+}))
+
 
 module.exports = router;
