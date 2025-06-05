6dcaada0c9c7fa20ca5517d812cf42e185c8a603
Mungi Cheon <c.mungi7421@gmail.com>
2025-06-06 01:26:31 +0900

    study: Post도메인 관련 ejs 파일 경로 수정

--- myBlog/controllers/postController.js
+++ myBlog/controllers/postController.js
@@ -4,18 +4,18 @@ const mainLayout = "../views/layouts/main.ejs";
 const getAllPost = async (req, res) => {
     const locals = { title: "Home" };
     const data = await postService.fetchAllPosts();
-    res.render("index", { locals, data, layout: mainLayout });
+    res.render("post/index", { locals, data, layout: mainLayout });
 };
 
 const getAboutPage = (req, res) => {
     const locals = { title: "About" };
-    res.render("about", { locals, layout: mainLayout });
+    res.render("post/about", { locals, layout: mainLayout });
 };
 
 const getPostDetail = async (req, res) => {
     const layout = postService.determineLayout(req);
     const data = await postService.fetchPostById(req.params.id);
-    res.render("post", { data, layout });
+    res.render("post/post", { data, layout });
 };
 
 module.exports = {
