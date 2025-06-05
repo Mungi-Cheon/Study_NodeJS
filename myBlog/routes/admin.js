8d6041cab0acc1dff18a2c3d3d629664fbbc881c
Mungi Cheon <c.mungi7421@gmail.com>
2025-06-05 18:33:35 +0900

    study : MyBlog Post 수정, 삭제 구현

--- myBlog/routes/admin.js
+++ myBlog/routes/admin.js
@@ -10,6 +10,24 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 
 /**
+ * Check Login
+ */
+const checkLogin = (req, res, next) =>{
+    const token = req.cookies.token;
+    if(!token){
+        res.redirect("/admin");
+    }else{
+        try{
+            const decoded = jwt.verify(token, jwtSecret);
+            req.userId = decoded.userId;
+            next();
+        }catch(error){
+            res.redirect("/admin");
+        }
+    }
+}
+
+/**
  * Admin Page
  * Get /admin
  */
@@ -65,13 +83,18 @@ router.post("/register", asyncHandler(async (req, res) =>{
  * Get all Posts
  * Get /allPosts
  */
-router.get("/allPosts", asyncHandler(async(req, res)=>{
+router.get("/allPosts", checkLogin, asyncHandler(async(req, res)=>{
     const locals = {
         title : "Posts"
     };
     
-    const data = await Post.find();
-    res.render("admin/allPosts", {locals, data, layout:adminLayout});
+    const data = await Post.find().sort({ createdAt: -1 });
+    const formattedData = data.map(post => ({
+    ...post._doc,
+    formattedDate: post.createdAt.toISOString().split('T')[0]  // "YYYY-MM-DD"
+}));
+
+    res.render("admin/allPosts", {locals, data:formattedData, layout:adminLayout});
 }));
 
 /**
@@ -82,5 +105,65 @@ router.get("/logout", (req, res) => {
     res.clearCookie("token");
     res.redirect("/");
 })
+
+/**
+ * Admin Add Get
+ * Get /add
+ */
+router.get("/add", checkLogin, asyncHandler(async(req, res) => {
+    const locals = {
+        title : "게시물 작성"
+    };
+    res.render("admin/add", {locals, layout: adminLayout});
+}))
+
+
+/**
+ * Add Post
+ * Post /add
+ */
+router.post("/add", checkLogin, asyncHandler(async (req, res) => {
+    const {title, body} = req.body;
+    if(!title || !body){
+        return res.send("'필수 항목목이 입력되지 않았습니다.'");
+    }
+    const post = await Post.create({title, body});
+    res.redirect("/allPosts");
+}));
+
+/**
+ * Admin Edit Get
+ * Get /edit/:id
+ */
+router.get("/edit/:id", checkLogin, asyncHandler(async (req, res) => {
+    const locals = {
+        title : "게시물 수정"
+    };
+    const data = await Post.findOne({_id: req.params.id});
+    res.render("admin/edit", {locals, data, layout: adminLayout});
+}));
+
+/**
+ * Admin Edit Post
+ * Put /edit/:id
+ */
+router.put("/edit/:id", checkLogin, asyncHandler(async (req, res) =>{
+    await Post.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        body: req.body.body,
+        createdAt: Date.now()
+    });
+    res.redirect("/allPosts");
+}));
+
+/**
+ * Admin Delete Post
+ * Delete /delete/:id
+ */
+router.delete("/delete/:id", checkLogin, asyncHandler(async (req, res) =>{
+    await Post.deleteOne({_id: req.params.id});
+    res.redirect("/allPosts");
+}));
+
 
 module.exports = router;
--- myBlog/routes/main.js
+++ myBlog/routes/main.js
@@ -3,6 +3,28 @@ const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs";
 const Post = require("../models/Post");
 const asyncHandler = require("express-async-handler");
+const adminLayout = "../views/layouts/admin.ejs";
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
+/**
+ * 레이아웃 결정
+ * @param {*} req 
+ * @returns 
+ */
+const determineLayout = (req) => {
+    const token = req.cookies?.token;
+
+    if (!token){
+        return mainLayout;  
+    } 
+    try {
+        jwt.verify(token, jwtSecret);
+        return adminLayout; // 토큰만 유효하면 adminLayout
+    } catch (e) {
+        return mainLayout; // 유효하지 않으면 mainLayout
+    }
+};
+
 
 
 router.get(["/","/home"], asyncHandler( async (req, res) => {
@@ -26,8 +48,9 @@ router.get("/about", (req, res) => {
  * Get /posts/:id
  */
 router.get("/posts/:id", asyncHandler( async (req, res) => {
+    const layout = determineLayout(req);
     const data = await Post.findOne({_id: req.params.id});
-    res.render("post", {data, layout: mainLayout});
+    res.render("post", {data, layout: layout});
 }))
 
 
